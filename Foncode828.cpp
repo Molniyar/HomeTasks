64e6b12e6822c640d18f8b949ad56c647f91d7af
Yaroslav Savin <molniyar11@gmail.com>
2026-03-09 17:02:17 +0400

    added task 828 from Foncode

---
+++ Foncode828.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/*
+int main() {
+	int x1;std::cin >> x1;
+	int y1;std::cin >> y1;
+	int x2;std::cin >> x2;
+	int y2;std::cin >> y2;
+
+	if (x1 > 0 && x2 > 0) std::cout << "right";
+	else if (x1 < 0 && x2 < 0) std::cout << "left";
+	else std::cout << "cross";
+
+	return 0;
+}
+//*/

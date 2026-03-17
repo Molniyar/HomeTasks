57a80d386eb40344826b043b8fba476d0c5643ea
Yaroslav Savin <molniyar11@gmail.com>
2026-03-17 21:51:58 +0400

    added task 1026 from foncode

---
+++ Foncode1026.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+int main() {
+	int size; std::cin >> size;
+	int width; std::cin >> width;
+
+	int s1 = 0;
+	int s2 = 0;
+
+	int read;
+	for (int i = 0; i < size - 1; i++) {
+		std::cin >> read;
+		s1 += read;
+		s2 += width - read;
+	}
+	if (std::abs(s1 - s2) > width || (size*width)&1) {
+		std::cout << "-1";
+	}
+	else std::cout << ((s2 - s1 + width) >> 1);
+}
--- Foncode1027.cpp
+++ Foncode1027.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-//*
+/*
 int main() {
 	std::string input; std::cin >> input;
 

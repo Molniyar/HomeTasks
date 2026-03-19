3d665d32083dac196863a00205968ee80eb989a2
Yaroslav Savin <molniyar11@gmail.com>
2026-03-19 20:27:51 +0400

    added task 1030 from Fonccode

---
+++ Foncode1030.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+//*
+int main() {
+	int arg1; std::cin >> arg1;
+	int arg2; std::cin >> arg2;
+
+	while (arg1 != arg2 && arg1 && arg2) {
+		//std::cout << "|" << arg1 << ' ' << arg2 << '\n';
+		if (arg1 > arg2) {
+			arg1 %= arg2;
+			continue;
+		}
+		arg2 %= arg1;
+	}
+	if (arg2 > arg1)arg1 = arg2;
+	arg2 = arg1;
+	//std::cout << arg1 << '\n';
+
+	for (int i = 1;i <= arg2 ; i++) {
+		if (arg1 % i == 0) {
+			//std::cout << i << '\n';
+			if (arg1/i <= i) {
+				std::cout << arg1/i;
+				return 0;
+			}
+		}
+	}
+}
+//*/
--- Foncode616.cpp
+++ Foncode616.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-//*
+/*
 int main() {
 	int size; std::cin >> size;
 

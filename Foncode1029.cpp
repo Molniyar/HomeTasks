d0c544f4dac4862b40c49b0d3e0bdf727d8ddf87
Yaroslav Savin <molniyar11@gmail.com>
2026-03-20 19:10:00 +0400

    added task 1029 from Foncode

---
+++ Foncode1029.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//*
+int main() {
+	int white; std::cin >> white;
+	int black; std::cin >> black;
+	std::cout << ((white & 1) ^ (black & 1) ? "Black" : "White");
+}
+//*/
--- Foncode1032.cpp
+++ Foncode1032.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
-//*
+/*
 int main() {
-	int in; std::cin >> in;
+	long in; std::cin >> in;
 	std::cout << in * (in - 1) * 2 + 1;
 }
 //*/

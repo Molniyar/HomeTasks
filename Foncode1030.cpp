8afcf1f6da4505bd6c08723b797f831ceb864a1a
Yaroslav Savin <molniyar11@gmail.com>
2026-03-20 18:47:25 +0400

    added task 1032 from Foncode

--- Foncode1030.cpp
+++ Foncode1030.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-//*
+/*
 int main() {
 	int arg1; std::cin >> arg1;
 	int arg2; std::cin >> arg2;
---
+++ Foncode1032.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+//*
+int main() {
+	int in; std::cin >> in;
+	std::cout << in * (in - 1) * 2 + 1;
+}
+//*/

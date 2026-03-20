33d04dd71863f329ae73d91a4b3663d9d8f7380a
Yaroslav Savin <molniyar11@gmail.com>
2026-03-20 19:53:13 +0400

    added task 90 from Foncode

--- Foncode1029.cpp
+++ Foncode1029.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-//*
+/*
 int main() {
 	int white; std::cin >> white;
 	int black; std::cin >> black;
-	std::cout << ((white & 1) ^ (black & 1) ? "Black" : "White");
+	std::cout << ((white & 1) ^ (black & 1) ? "White" : "Black");
 }
 //*/
---
+++ Foncode90.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <map>
+
+//*
+int main() {
+	int size; std::cin >> size;
+
+	std::map<std::string, int> prices;
+
+	std::string name;
+	int price;
+	for (int i = 0; i < size; i++) {
+		std::cin >> name;
+		std::cin >> price;
+		if (prices.find(name) == prices.end()) {
+			prices[name]=price;
+			continue;
+		}
+		if (prices.at(name) > price) {
+			prices.at(name) = price;
+		}
+	}
+
+	for (auto item: prices) {
+		std::cout << item.first << ' ' << item.second << '\n';
+	}
+}
+//*/

618965b7042b61bbe316c8ce4b7ca88e439f8b7d
Yaroslav Savin <molniyar11@gmail.com>
2026-03-28 16:21:28 +0400

    added task 1037 from Foncode

---
+++ Foncode1037.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <map>
+
+int main() {
+	int size; std::cin >> size;
+
+	std::map<int, int> divs;
+	int read;
+	for (int i = 0; i < size; i++) { 
+		std::cin >> read;
+		for (int j = 2; (read/j) >= j; j++) {
+			if (read % j == 0) {
+				divs[j]++;
+				while (read % j == 0) read /= j;
+			}
+		}
+		if (read != 1) divs[read]++;
+	}
+
+	int result = 0;
+	for (auto i:divs) {
+		std::cout << i.first << ' ' << i.second << '\n';
+		if (i.second > result) result = i.second;
+	}
+
+	std::cout << result;
+}
--- Foncode1039.cpp
+++ Foncode1039.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-//*
+/*
 int mul(int a, int b) {
 	return (a * b) % 10;
 }
